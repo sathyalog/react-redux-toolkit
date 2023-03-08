9af0f7d64d95b47e39ac63617f952180e8e8a440
Sathya Vakacharla <sathyavakacharla@192.168.1.107>
2023-03-08 12:26:27 +0000

    fetching users from api

--- src/features/user/UserView.jsx
+++ src/features/user/UserView.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers } from './userSlice';
+
 
 function UserView() {
+    const user = useSelector((state) => state.user)
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchUsers());
+    },[])
   return (
     <div>
-        <h2>List of users - </h2>
-        
+        <h2>List of users</h2>
+        {user.loading && <div>Loading..</div>}
+        {!user.loading && user.error ?<div>Error{user.error}</div> : null}
+        {!user.loading && user.users.length ? 
+        <ul>
+            {user.users.map(user => (
+                <li key={user.id}>{user.name}</li>
+            ))}
+            
+        </ul>: null
+        }
     </div>
   )
 }
